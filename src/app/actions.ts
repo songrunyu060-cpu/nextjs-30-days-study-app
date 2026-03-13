692ad692d8a7de7fd30047121f0176e3dbe6c0a3
宋润宇 <songrunyu@songrunyudeAir.lan>
2026-03-13 11:09:20 +0800

    feat: initial commit with nextjs 16 and prisma

---
+++ src/app/actions.ts
@@ -0,0 +1,35 @@
+"use server";
+import { neon } from "@neondatabase/serverless";
+import { revalidatePath } from "next/cache";
+
+type User = {
+  id: number;
+  email: string;
+  name: string | null;
+};
+
+function getSql() {
+  const databaseUrl = process.env.DATABASE_URL;
+  if (!databaseUrl) throw new Error("DATABASE_URL is not set");
+  return neon(databaseUrl);
+}
+
+export async function getData() {
+  const sql = getSql();
+  return await sql`select 1 as ok`;
+}
+
+export async function createRandomUser(_formData: FormData): Promise<void> {
+  const sql = getSql();
+  const email = `test-${crypto.randomUUID()}@test.com`;
+  const name = "NextJS 16 Learner";
+
+  await sql`insert into "User" (email, name) values (${email}, ${name})`;
+  revalidatePath("/");
+}
+
+//  查找用户
+export async function findUsers(): Promise<User[]> {
+  const sql = getSql();
+  return (await sql`select * from "User"`) as unknown as User[];
+}
